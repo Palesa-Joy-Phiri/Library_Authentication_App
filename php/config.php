4deb8be97b5c3fb90310e97c2ea5cfb4224ba869
Palesa-Joy-Phiri <95339260+Palesa-Joy-Phiri@users.noreply.github.com>
2022-10-30 22:24:02 +0200

    changed database
    php mysql not working on localhost using freemysqldatabase

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define ('DB_NAME', 'sql8528597');
-define ('DB_USER', 'sql8528597');
-define ('DB_PASSWORD','LlM2er6piT');
+define ('DB_NAME', 'sql8530600');
+define ('DB_USER', 'sql8530600');
+define ('DB_PASSWORD','sU1PLnbIjJ');
 define ('DB_HOST', 'sql8.freesqldatabase.com');
 
 $connection = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
